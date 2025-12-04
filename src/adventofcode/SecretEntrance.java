940cd99f8cf102bc4d71f4346d34a1ee8aa41f83
Michał Przepiórka <michal.przepiorka0@gmail.com>
2025-12-04 21:32:39 +0100

    First day, part II

--- src/adventofcode/SecretEntrance.java
+++ src/adventofcode/SecretEntrance.java
@@ -11,6 +11,7 @@ public class SecretEntrance {
         int dialBeginPosition = 50;
         int currentDialPosition = dialBeginPosition;
         int zeroCount = 0;
+        int allZeroCount = 0;
 
         fillListWithSequences(rotationSequenceData, rotationSequenceList);
 
@@ -18,6 +19,7 @@ public class SecretEntrance {
         for (String sequence : rotationSequenceList) {
             switch (sequence.charAt(0)) {
                 case 'L', 'R' -> {
+                    allZeroCount += countZeroHits(currentDialPosition, sequence, 100);
                     currentDialPosition = doMove(currentDialPosition, sequence, 100);
                     if (currentDialPosition == 0) {
                         zeroCount++;
@@ -29,6 +31,21 @@ public class SecretEntrance {
 
         System.out.println("Final position: " + currentDialPosition);
         System.out.println("Zero count: " + zeroCount);
+        System.out.println("All zero count: " + allZeroCount);
+    }
+
+    private static int countZeroHits(int startPos, String sequence, int max) {
+        char moveDirection = sequence.charAt(0);
+        int rotationQuantity = Integer.parseInt(sequence.substring(1));
+        int hits = 0;
+
+
+        for (int i = 1; i <= rotationQuantity; i++) {
+            int pos = moveDirection == 'L' ? (startPos - i) % max : (startPos + i) % max;
+            if (pos < 0) pos += max;
+            if (pos == 0) hits++;
+        }
+        return hits;
     }
 
     private static int doMove(int currentDialPosition, String sequence, int max) {
